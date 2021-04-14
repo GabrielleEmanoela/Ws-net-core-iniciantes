6ea75f04b1722399641655264c54532cc4c444ed
˜Gabrielle <biela.a09@gmail.com>
2021-04-14 16:58:17 -0300

    Modificando botão

--- src/FilmesCRUDRazor/Models/Film.cs
+++ src/FilmesCRUDRazor/Models/Film.cs
@@ -1,15 +1,22 @@
 using System;
-
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 namespace FilmesCRUDRazor.Models
 {
     public class Film
     {
-
         public int FilmId { get; set; }
+
         public string Title { get; set; }
+        [Display(Name = "Release date of")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime DateLaunch { get; set; }
         public string Gender { get; set; }
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal()18,2")]
         public decimal Price { get; set; }
+
+
 
     }
 }
